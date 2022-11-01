21d3f75ddc3bb91b8eef84ce18f7d3a5059710a6
Nayem <nayem.ceed@gmail.com>
2022-11-01 11:06:02 -0700

    new line added

---
+++ index.js
@@ -0,0 +1,11 @@
+var myMap= new Map();
+
+myMap.set("Key1","Bangladesh")
+myMap.set("Key2","India")
+myMap.set("Key3","Pakistan")
+myMap.set("Key4","Nepal")
+myMap.set("Key5","Bhutan")
+myMap.set("Key6","Maldivs")
+myMap.set("Key7","Thailand")
+
+console.log(myMap.values());
